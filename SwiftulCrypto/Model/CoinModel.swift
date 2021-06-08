39ba70944039af1de45e76958fe36da6a1c3dc72
daimucn <laosi404@qq.com>
2021-06-08 10:05:10 +0800

    CoinModel Check

--- SwiftulCrypto/Model/CoinModel.swift
+++ SwiftulCrypto/Model/CoinModel.swift
@@ -60,7 +60,9 @@ struct CoinModel: Identifiable, Codable{
     let currentPrice: Double
     let marketCap, marketCapRank, fullyDilutedValuation: Double?
     let totalVolume, high24H, low24H: Double?
-    let priceChange24H, priceChangePercentage24H, marketCapChange24H, marketCapChangePercentage24H: Double?
+    let priceChange24H, priceChangePercentage24H: Double?
+    let marketCapChange24H: Double?
+    let marketCapChangePercentage24H: Double?
     let circulatingSupply, totalSupply, maxSupply, ath: Double?
     let athChangePercentage: Double?
     let athDate: String?
